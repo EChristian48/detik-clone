6c913cb02c88b1ae2d0ab9b059badc0e19881d3d
Erick Christian <erickchristian48@gmail.com>
2021-12-13 02:16:04 +0700

    style: material ui base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
+import theme from '~/material.theme'
+import '~/styles/fonts.css'
+import '~/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
