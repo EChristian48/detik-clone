5f968e1e960b113bcf38cdf24488a297a99e90e4
Erick Christian <erickchristian48@gmail.com>
2021-12-13 02:17:49 +0700

    feat: styled image component

---
+++ components/StyledImage.tsx
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material'
+import { ImgHTMLAttributes } from 'react'
+
+const StyledImage = styled('img')<ImgHTMLAttributes<HTMLImageElement>>(
+  ({}) => ({})
+)
+
+export default StyledImage
