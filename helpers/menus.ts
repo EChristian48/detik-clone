5959f2eae9ef256108b5fb37992010d384545a30
Erick Christian <erickchristian48@gmail.com>
2021-12-13 02:16:24 +0700

    feat: menus helper

---
+++ helpers/menus.ts
@@ -0,0 +1,21 @@
+export const menus = [
+  'detikNews',
+  'detikFinance',
+  'detikHot',
+  'detikInet',
+  'detikSport',
+  'detikOto',
+  'detikTravel',
+  'detikFood',
+  'detikHealth',
+  'Wolipop',
+  '20Detik'
+]
+
+export const mobileMenus = [
+  'berbuatbaik.id',
+  'Terpopuler',
+  'Edukasi',
+  'Hikmah',
+  'Indeks'
+]
