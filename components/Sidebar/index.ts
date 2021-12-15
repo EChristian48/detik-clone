d059132f9448be84ae12503314c2b83c509d9bd2
Erick Christian <erickchristian48@gmail.com>
2021-12-15 13:39:01 +0700

    feat: Sidebar component

---
+++ components/Sidebar/index.ts
@@ -0,0 +1,5 @@
+export { default as BeritaTerpopuler } from './BeritaTerpopuler'
+export { default as DesktopSidebar } from './DesktopSidebar'
+export { default as DoYourMagic } from './DoYourMagic'
+export { default as TagPopuler } from './TagPopuler'
+
