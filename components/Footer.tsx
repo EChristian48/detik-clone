087df4da80bbe3af8e0db436fc044e456262dfb7
Erick Christian <erickchristian48@gmail.com>
2021-12-15 13:36:37 +0700

    feat: Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,153 @@
+import {
+  Facebook,
+  Instagram,
+  LinkedIn,
+  Twitter,
+  YouTube
+} from '@mui/icons-material'
+import { Box, Container, Grid, Stack, Typography } from '@mui/material'
+import logo from '~/assets/logo.png'
+import {
+  categories,
+  footerMenus,
+  informasi,
+  jaringanMedia,
+  layanan
+} from '~/helpers'
+import theme from '~/material.theme'
+import { StyledImage } from '.'
+
+export default function Footer() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: '#e6e6e6',
+        paddingY: theme.spacing(6),
+        borderTop: `4px solid ${theme.palette.primary.main}`
+      }}>
+      <Container>
+        <Grid container columnSpacing={1}>
+          {/* Logos and socials */}
+          <Grid item xs={12} md={3}>
+            <Stack spacing={2}>
+              <StyledImage
+                src={logo.src}
+                sx={{
+                  maxWidth: { xs: '50%', sm: '80%' },
+                  display: 'block',
+                  marginX: 'auto'
+                }}
+              />
+
+              <Stack
+                direction="row"
+                flexWrap="wrap"
+                justifyContent="center"
+                gap={1}>
+                {footerMenus.map((item, index) => (
+                  <Typography fontSize={12} key={index}>
+                    {item}
+                  </Typography>
+                ))}
+              </Stack>
+
+              <Stack
+                direction="row"
+                flexWrap="wrap"
+                justifyContent="center"
+                gap={1}>
+                <Facebook />
+                <Twitter />
+                <Instagram />
+                <LinkedIn />
+                <YouTube />
+              </Stack>
+
+              <Typography textAlign="center" fontSize={12}>
+                Copyright @ 2021 detikcom, All right reserved
+              </Typography>
+            </Stack>
+          </Grid>
+
+          {/* Categories */}
+          <Grid
+            item
+            md={3}
+            sx={{
+              [theme.breakpoints.down('md')]: {
+                display: 'none'
+              }
+            }}>
+            <Typography fontWeight={700} fontSize={14} marginBottom={2}>
+              Kategori
+            </Typography>
+            <Stack flexWrap="wrap" maxHeight={200} spacing={1 / 2}>
+              {categories.map((item, index) => (
+                <Typography key={index} fontSize={12}>
+                  {item}
+                </Typography>
+              ))}
+            </Stack>
+          </Grid>
+          <Grid
+            item
+            md={2}
+            sx={{
+              [theme.breakpoints.down('md')]: {
+                display: 'none'
+              }
+            }}>
+            <Typography fontWeight={700} fontSize={14} marginBottom={2}>
+              Layanan
+            </Typography>
+            <Stack flexWrap="wrap" maxHeight={200} spacing={1 / 2}>
+              {layanan.map((item, index) => (
+                <Typography key={index} fontSize={12}>
+                  {item}
+                </Typography>
+              ))}
+            </Stack>
+          </Grid>
+          <Grid
+            item
+            md={2}
+            sx={{
+              [theme.breakpoints.down('md')]: {
+                display: 'none'
+              }
+            }}>
+            <Typography fontWeight={700} fontSize={14} marginBottom={2}>
+              Informasi
+            </Typography>
+            <Stack flexWrap="wrap" maxHeight={200} spacing={1 / 2}>
+              {informasi.map((item, index) => (
+                <Typography key={index} fontSize={12}>
+                  {item}
+                </Typography>
+              ))}
+            </Stack>
+          </Grid>
+          <Grid
+            item
+            md={2}
+            sx={{
+              [theme.breakpoints.down('md')]: {
+                display: 'none'
+              }
+            }}>
+            <Typography fontWeight={700} fontSize={14} marginBottom={2}>
+              Jaringan Media
+            </Typography>
+            <Stack flexWrap="wrap" maxHeight={200} spacing={1 / 2}>
+              {jaringanMedia.map((item, index) => (
+                <Typography key={index} fontSize={12}>
+                  {item}
+                </Typography>
+              ))}
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  )
+}
